677f2fa7571029bb34ddbbfd3ffa85967b35bb04
Pax-Newman <newmanp@wwu.edu>
2024-06-29 23:36:05 -0700

    added WeSplit project

---
+++ projects/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Pax Newman on 6/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, Me!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
