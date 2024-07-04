376a24cbfd10a80b34c24676db8a0f494ba21c2d
Pax-Newman <newmanp@wwu.edu>
2024-07-03 22:05:56 -0700

    added GuessTheFlag project

---
+++ projects/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Pax Newman on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
